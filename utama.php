101eb740e4d31eec6094207859455ca495e32263
ilyasganteng <ilyasden17@gmail.com>
2016-12-30 08:16:01 +0700

    iki tampilan utama rek edit pis tipis

---
+++ utama.php
@@ -0,0 +1,8 @@
+<div id="content">
+	<h2>Selamat Datang di Sistem Kepegawaian PT. UBL</h2>
+	<p>Sistem Kepegawaian ini dibuat untuk memenuhi tugas UAS matakuliah Pemrograman Web 2 di 
+	<a href="http://www.bl.ac.id">Universitas Budi Luhur</a>, Dosen: Achmad Solichin.</p>
+	<p>Dalam Sistem Kepegawaian ini, Anda dapat <a href="index.php?page=input">menginput</a> data pegawai, 
+	<a href="index.php?page=tampil">melihat</a> data pegawai, dan mengedit serta delete data pegawai</p>
+	<p>Have a nice day!</p>
+</div>
