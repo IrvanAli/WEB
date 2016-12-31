182b087aea364c9e6300264b7fc5487fd06c7806
ilyasganteng <ilyasden17@gmail.com>
2016-12-31 20:08:27 +0700

    ini about tambahan ben rodok sangar titik

---
+++ about.php
@@ -0,0 +1,10 @@
+<div id="content">
+	<h2>Tentang Pembuat Program</h2>
+	<p>
+	Nim : <b>0411500415</b><br/>
+	Nama : <b>Achmad Solichin</b><br/>
+	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	Kelompok : <b>KB</b><br/>
+	Email : <b><a href="mailto:achmatim@gmail.com">achmatim@gmail.com</a></b><br/>
+	</p>
+</div>
