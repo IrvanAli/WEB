2900d44a68ecb201005e3ff233659372d99f9325
DESKTOP-G6FU8JD\Irvan Ali Usman <irvanaliusman@gmail.com>
2017-01-01 11:24:58 +0700

    merubah kata kata di tampilan home

--- utama.php
+++ utama.php
@@ -1,8 +1,5 @@
 <div id="content">
-	<h2>Selamat Datang di Sistem Kepegawaian PT. UBL</h2>
-	<p>Sistem Kepegawaian ini dibuat untuk memenuhi tugas UAS matakuliah Pemrograman Web 2 di 
-	<a href="http://www.bl.ac.id">Universitas Budi Luhur</a>, Dosen: Achmad Solichin.</p>
-	<p>Dalam Sistem Kepegawaian ini, Anda dapat <a href="index.php?page=input">menginput</a> data pegawai, 
-	<a href="index.php?page=tampil">melihat</a> data pegawai, dan mengedit serta delete data pegawai</p>
+	<h2>Selamat Datang di Sistem Karyawan PT.Makmur</h2>
+	<p>Sistem Karyawan ini dibuat untuk memenuhi tugas Akhir Semester matakuliah Pemrograman Web 2.
 	<p>Have a nice day!</p>
 </div>
