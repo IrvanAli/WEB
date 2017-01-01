712d0af2fedb44383b547cbb7873bc0cfd961726
DESKTOP-G6FU8JD\Irvan Ali Usman <irvanaliusman@gmail.com>
2017-01-01 10:34:50 +0700

    mohon di import lagi yang pegawai.sql ini karena di dalam itu ada table baru untuk login udah ada seasoin untuk login dan logout

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+	session_start();
+	if(!isset($_SESSION['username'])) {
+		header('location:login.php'); 
+	} else { 
+		$username = $_SESSION['username']; 
+	}
+?>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Sistem Karyawan</title>
@@ -13,6 +21,7 @@
 	<a href="index.php?page=input">Input Data Pegawai</a> 
 	<a href="index.php?page=tampil">Tampil Data Pegawai</a> 
 	<a href="index.php?page=about">About</a> 
+	<a href="index.php?page=logout">logout</a> 
 </div>
 
 <?php
@@ -22,9 +31,8 @@ switch ($page) {
 	case 'edit'	: include "edit_peg.php"; break;
 	case 'delete' : include "delete_peg.php"; break;
 	case 'tampil' : include "tampil.php"; break;
-	case 'foto' : include "foto.php"; break;
+	case 'logout' : include "logout.php"; break;
 	case 'about' : include "about.php"; break;
-	case 'main' :
 default : include 'utama.php';	
 
 }
