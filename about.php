61c88dbf8aa7af786134a3cf35d8ab531e281f81
DESKTOP-G6FU8JD\Irvan Ali Usman <irvanaliusman@gmail.com>
2017-01-01 11:25:22 +0700

    merubah tentang edit , tinggal ngisi nama , sama nim kalian

--- about.php
+++ about.php
@@ -1,10 +1,23 @@
 <div id="content">
-	<h2>Tentang Pembuat Program</h2>
+	<h2>Tentanng Pembuat Program </h2>
 	<p>
-	Nim : <b>0411500415</b><br/>
-	Nama : <b>Achmad Solichin</b><br/>
+	Nim : <b>E31150180</b></br>
+	Nama : <b>Irvan Ali Usman</b><br/>
 	Matakuliah : <b>Pemrograman Web 2</b><br/>
-	Kelompok : <b>KB</b><br/>
-	Email : <b><a href="mailto:achmatim@gmail.com">achmatim@gmail.com</a></b><br/>
+	</p>
+	<p>
+	Nim : <b>.......</b><br/>
+	Nama : <b>Nurudin Ilyas</b><br/>
+	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	</p>
+	<p>
+	Nim : <b>.......</b><br/>
+	Nama : <b>Fitri Andayani</b><br/>
+	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	</p>
+	<p>
+	Nim : <b>.......</b><br/>
+	Nama : <b>Yodion Wirawanda</b><br/>
+	Matakuliah : <b>Pemrograman Web 2</b><br/>
 	</p>
 </div>
