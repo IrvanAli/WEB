5ca494d5b7b9de91044f17097f32e55864766670
ilyasganteng <ilyasden17@gmail.com>
2017-01-01 23:01:22 +0700

    ini tak ubah tambahi jenenge dewe

--- about.php
+++ about.php
@@ -1,10 +1,10 @@
 <div id="content">
 	<h2>Tentang Pembuat Program</h2>
 	<p>
-	Nim : <b>0411500415</b><br/>
-	Nama : <b>Achmad Solichin</b><br/>
-	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	Nim : <b>E31150560</b><br/>
+	Nama : <b>Nuruddin ILyas</b><br/>
+	Matakuliah : <b>Pemrograman Web </b><br/>
 	Kelompok : <b>KB</b><br/>
-	Email : <b><a href="mailto:achmatim@gmail.com">achmatim@gmail.com</a></b><br/>
+	Email : <b><a href="mailto:ilyasden17@gmail.com">ilyasden17@gmail.com</a></b><br/>
 	</p>
 </div>
