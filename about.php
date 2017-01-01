50759b3f9c870f962c0402cb7ae9e69d4cd2a687
DESKTOP-G6FU8JD\Irvan Ali Usman <irvanaliusman@gmail.com>
2017-01-02 01:23:55 +0700

    tambahkan sendiri NIM kalian , email udah tak kasik

--- about.php
+++ about.php
@@ -1,26 +1,27 @@
 <div id="content">
 	<h2>Tentang Pembuat Program </h2>
 	<p>
- 
 	Nim : <b>E31150560</b><br/>
 	Nama : <b>Nuruddin ILyas</b><br/>
 	Matakuliah : <b>Pemrograman Web </b><br/>
-	Kelompok : <b>KB</b><br/>
 	Email : <b><a href="mailto:ilyasden17@gmail.com">ilyasden17@gmail.com</a></b><br/>
-
+	</p>
+	<p>
 	Nim : <b>E31150180</b></br>
 	Nama : <b>Irvan Ali Usman</b><br/>
 	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	Email : <b><a href="irvanaliusman@gmail.com">irvanaliusman@gmail.com</a></b><br/>
 	</p>
 	<p>
 	Nim : <b>.......</b><br/>
 	Nama : <b>Fitri Andayani</b><br/>
 	Matakuliah : <b>Pemrograman Web 2</b><br/>
+	Email : <b><a href="fitriandayani0697@gmail.com">fitriandayani0697@gmail.com</a></b><br/>
 	</p>
 	<p>
 	Nim : <b>.......</b><br/>
 	Nama : <b>Yodion Wirawanda</b><br/>
 	Matakuliah : <b>Pemrograman Web 2</b><br/>
-       
+       	Email : <b><a href="yodionwanda@gmail.com">yodionwanda@gmail.com</a></b><br/>
 	</p>
 </div>
